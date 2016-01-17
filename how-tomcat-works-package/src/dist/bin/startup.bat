abe8fd8ba5cc8783125097eb8f69ea81ada93de1
zhiyong zhu <zzycjcg@qq.com>
2016-01-17 22:17:04 +0800

    修改shell脚本问题

--- how-tomcat-works-package/src/dist/bin/startup.bat
+++ how-tomcat-works-package/src/dist/bin/startup.bat
@@ -4,4 +4,4 @@ set "APP_HOME=%cd%"
 set "JAVA_OPTS=-server -Dapp.home=%APP_HOME%"
 set "CLASSPATH=%APP_HOME%\lib\*;%APP_HOME%\conf\*"
 						
-java -classpath %CLASSPATH% %JAVA_OPTS% com.github.zzycjcg.simple.httpserver.Main
+java %JAVA_OPTS% -classpath %CLASSPATH% com.github.zzycjcg.simple.httpserver.Main
--- how-tomcat-works-package/src/dist/bin/startup.sh
+++ how-tomcat-works-package/src/dist/bin/startup.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+cd ..
+APP_HOME=`pwd`
+JAVA_OPTS="-server -Dapp.home=$APP_HOME"
+CLASSPATH="$APP_HOME/lib/*:$APP_HOME/conf/*"
+                        
+java $JAVA_OPTS -classpath $CLASSPATH com.github.zzycjcg.simple.httpserver.Main &
